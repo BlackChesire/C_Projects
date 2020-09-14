8350efe95da69208945ef0dae50cd409b680f968
BlackChesire <asafbs177@gmail.com>
2020-09-14 19:54:30 +0300

    update

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 
 int main() {
     int number = 0;
+    char str[10];
+    printf("Enter a name:\n");
+    scanf("%s", str);
     printf("Enter a number:\n");
     scanf("%d", &number); // &number means a pointer to number in the memory
-
+    printf("hello, %s:\n", str);
     if (number % 2 == 0) {
         printf("Your number is even!\n");
     } else if (number > 10 && number % 2 !=0) {
