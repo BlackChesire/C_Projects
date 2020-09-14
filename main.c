65f0f28bf1d19db2689437c7f0b8ad10ac13afbf
BlackChesire <asafbs177@gmail.com>
2020-09-14 19:27:25 +0300

    update

--- main.c
+++ main.c
@@ -1,2 +1,23 @@
-#include <stdio.h>
-int x = 5;
+#include <stdio.h> //in order to use input & output
+
+int main() {
+    int number = 0;
+    printf("Enter a number:\n");
+    scanf("%d", &number); // &number means a pointer to number in the memory
+
+    if (number % 2 == 0) {
+        printf("Your number is even!\n");
+    } else if (number > 10 && number % 2 !=0) {
+        printf("Your number is ODD and greater than 10!\n");
+    }
+      else if (number % 2 != 0){
+          printf("your number is Odd");
+      }
+      else {
+        printf("Your number is ODD and less than or equal to 10\n");
+    }
+    return 0;
+    }
+
+    
+    
