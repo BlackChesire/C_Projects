93d07a0fcdb9a0e161e9f96db69ed97fcc106009
BlackChesire <asafbs177@gmail.com>
2020-09-24 18:52:05 +0300

    pointers

--- pointers.c
+++ pointers.c
@@ -2,6 +2,7 @@
 int main(){
     int num = 5; 
     int* num_pointer = &num; // new pointer 
+    *num_pointer = 6; // direfernce !
     // unsigned char* = num_adderss = (unsigned char*) &num; > we will use this to move 1 byte forward  
     int** pointer_to_pointer_to_num = &num_pointer;
     printf("the number is = %d, the pointer to num is = %p\n", num,num_pointer);
