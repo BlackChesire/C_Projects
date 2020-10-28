c46be45d1b81c456d711430a18b690067686174d
Avichai-Aziz <avichai.aziz@gmail.com>
2020-10-28 18:16:26 +0200

    update

--- homework.c
+++ homework.c
@@ -11,9 +11,41 @@ int main()
     if (scanf("(%lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf)",&house_cost, &down_payment, &initial_savings, &saving_annual_rate, 
     &mortgage_annual_rate, &initial_monthly_salary, &fraction_house_buying, &raise_fraction, &monthly_rent) == 9) {
     
-    double final_house_cost = 0, current_savings = initial_savings, current_monthly_salary = initial_monthly_salary;
+    double final_house_cost = 0;
+    double current_savings = initial_savings;
+    double current_monthly_salary = initial_monthly_salary;
+    double own_furtune_needed = (house_cost * down_payment);
+    double house_payment = (house_cost - current_savings); //the rest payment for the house
+    double salary_for_houses = (current_monthly_salary *  fraction_house_buying); //included the rent and the salary of the wanted house 
+    double salary_to_save = 0; //the part of salary for the house wants to buy
+    int count_months = 0;
+    int count_years = 0;
 
+    if (current_savings != own_furtune_needed) {
+        
+        while (current_savings != own_furtune_needed) {
+            salary_to_save = (salary_for_houses - monthly_rent); //the part of salary for the house wants to buy
+            current_savings += salary_to_save; //add the saved salary for the house, to the savings
+            ++count_months;
+
+            if (count_months == 12) {
+                ++count_years;
+                count_months = 0;
+            }
+
+
+
+
+        }
+    }
+
+    else
+    {
+        current_savings = 0; //the current savings is 0 because we pay it for the house
+        
+    }
     
+
 
 
 
