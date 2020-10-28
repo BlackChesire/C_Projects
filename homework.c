68f4cdf3e007c7261e86a46f5c71de320b50c81a
BlackChesire <asafbs177@gmail.com>
2020-10-28 16:42:26 +0200

    pushed int out

--- homework.c
+++ homework.c
@@ -1,15 +1,6 @@
 //Avicahi Aziz 316373497 & Asaf Ben Shabat 312391774
 #include <stdio.h> 
 int main()
-{
-    double house_cost = 0,down_payment = 0,initial_savings=0, saving_annual_rate=0, mortgage_annual_rate=0;
-    double fraction_house_buying = 0, monthly_rent = 0;
-    double intial_monthly_salary=0, raise_fraction=0;
-    printf("Enter (House-cost, down-payment, savings, savings-annual-rate, mortgage-annual-rate, salary, fraction-saving, annual-raise, house-rent):\n");
-    scanf("(%lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf)",&house_cost, &down_payment, &initial_savings, &saving_annual_rate, 
-    &mortgage_annual_rate, &intial_monthly_salary, &fraction_house_buying, &raise_fraction, &monthly_rent);
-
-
     int house_rent(double monthly_rent)
     {
         for(int i=0; i < monthly_rent; i++)
@@ -19,6 +10,16 @@ int main()
         }
         return 0;
     }
+{
+    double house_cost = 0,down_payment = 0,initial_savings=0, saving_annual_rate=0, mortgage_annual_rate=0;
+    double fraction_house_buying = 0, monthly_rent = 0;
+    double intial_monthly_salary=0, raise_fraction=0;
+    printf("Enter (House-cost, down-payment, savings, savings-annual-rate, mortgage-annual-rate, salary, fraction-saving, annual-raise, house-rent):\n");
+    scanf("(%lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf)",&house_cost, &down_payment, &initial_savings, &saving_annual_rate, 
+    &mortgage_annual_rate, &intial_monthly_salary, &fraction_house_buying, &raise_fraction, &monthly_rent);
+
+
+
     printf("After...\n");
     printf("After..\n");
     printf("Actual payment..\n");
