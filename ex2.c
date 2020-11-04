c0d97e81634b46ff1ccdea05f43c9189d6a66d35
BlackChesire <asafbs177@gmail.com>
2020-11-04 14:58:26 +0200

    new exerisce

---
+++ ex2.c
@@ -0,0 +1,21 @@
+// Asaf Ben Shabd & Avichai Aziz
+#include <stdio.h>
+#include <stdbool.h>
+int hash_arr(const *char){
+    unsigned long h = 5381;
+    //TODO  
+    return h;
+}
+int hash_ptr(const *char){
+    unsigned long h = 5381;
+    //TODO
+    return h;
+}
+bool is_legal_word(const *char){
+    //todo
+    return True;
+}
+int main(){
+
+    return 0;
+}
