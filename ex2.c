377eb937d9ac8df4e529984a9f67ab3226904112
BlackChesire <asafbs177@gmail.com>
2020-11-04 16:14:01 +0200

    sarel c

--- ex2.c
+++ ex2.c
@@ -1,17 +1,17 @@
 // Asaf Ben Shabt & Avichai Aziz 316373497
 #include <stdio.h>
 #include <stdbool.h>
-int hash_arr(const *char){
+int hash_arr(const char* string){
     unsigned long h = 5381;
     //TODO  
     return h;
 }
-int hash_ptr(const *char){
+int hash_ptr(const char* string){
     unsigned long h = 5381;
     //TODO
     return h;
 }
-bool is_legal_word(const *char){
+bool is_legal_word(const char* words){
     //TODO
         return True;
 
