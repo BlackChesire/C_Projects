b0adb6a73e33d002a22dcbe1d30d83a37b503000
BlackChesire <asafbs177@gmail.com>
2020-12-06 05:01:07 +0200

    HEADER file + num_ports + prostatecreat + prostatinit

--- Homework/ex3/ProStat.c
+++ Homework/ex3/ProStat.c
@@ -2,29 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/*
-int num_prots(char* fasta_file_name){ // TODO
-   int sumofprots;
-   FILE* fp = fopen(fasta_file_name,"r");
-   if (fp == NULL){
-       printf("Failed to read the fasta file\n");
-       return -1;
-   }
-   while (fscanf(fp) == 1)
-   {
-       sumofprots++;
-   }
-   return sumofprots;
-    
-} */
+int num_prots(char* fasta_file_name){ // WORKING
+  char line[256];
+    int num_of_prots = 0;
+    FILE* file = fopen(fasta_file_name, "r");
+    while (fgets(line, sizeof(line), file)) {
+        if (line[0] == '>')
+        {
+            num_of_prots++;
+        }
+        
+    }
+    fclose(file);
+    return num_of_prots;
+} 
 
-ProtStats* ProtStatsCreate(char* protname,int protlength){
+ProtStats* ProtStatsCreate(char* protname,int protlength){ //WORKING
   ProtStats *prot = (ProtStats*)calloc(sizeof(ProtStats),1);
   strcpy(prot->name,protname);
   prot->length = protlength;
   return prot;
 }
-void ProtStatsInit(char* protname,int protLength,ProtStats* protID){ //not sure !
+void ProtStatsInit(char* protname,int protLength,ProtStats* protID){ //not sure ! 
     strcpy(protID->name,protname);
     protID->length = protLength;
 }
--- Homework/ex3/main.c
+++ Homework/ex3/main.c
@@ -2,8 +2,4 @@
 #include <stdio.h>
 
 int main(){
-    char protname[] = "mmm";
-    int protlength = 31;
-    ProtStats* m = ProtStatsCreate(protname,protlength);
-     printf("%s",m->name);
 }
